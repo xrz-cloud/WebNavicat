90a8007df2263aeac8b40a8df84c58c4c0e0ed7a
xrz-cloud <3545270592@qq.com>
2023-01-08 21:56:26 +0800

    更新

---
+++ pages/blog/CopyRight.tsx
@@ -0,0 +1,22 @@
+function CR() {
+  return (
+    <>
+      <footer className="footer">
+        <p>
+          <strong>本文作者</strong>：xrz
+          <br />
+          <strong>文章来源</strong>：https://www.xrzyun.top
+          <br />
+          <strong>版权声明</strong>：本博客所有文章除特别声明外，均采用
+          <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">
+            BY-NC-SA
+          </a>
+          许可协议。转载请注明出处！免授权范围：<code>*.xrzyun.top</code>
+          及节点中域名。
+        </p>
+      </footer>
+    </>
+  );
+}
+
+export default CR;
