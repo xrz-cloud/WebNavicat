0cf2d0a29488e2cb1994127cc57a59ecd09ce1fa
xrz-cloud <62951481+xrz-cloud@users.noreply.github.com>
2023-08-29 21:22:38 +0800

    add headers

--- next.config.mjs
+++ next.config.mjs
@@ -35,6 +35,19 @@ const config = {
           },
         ],
       },
+      {
+        source: '/_next/static/(.*)',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin',
+          },
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'require-corp',
+          },
+        ],
+      },
     ]
   },
   experimental: {
